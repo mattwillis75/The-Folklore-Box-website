e788703291356d6326c2520b842ec4f0da501447
mattwillis75 <thefolklorebox@gmail.com>
2026-07-19 14:46:40 +0100

    Add files via upload

--- pins.js
+++ pins.js
@@ -1,26 +1,87 @@
 const savedPins = [
   {
-    "lat": 362.2373278441525,
-    "lng": 681.2909988693019,
-    "title": "Black Shuck",
-    "category": "Cryptid",
-    "image": "https://via.placeholder.com/400x250",
-    "desc": "In English folklore, Black Shuck, Old Shuck, Old Shock or simply Shuck is the name given to a ghostly black dog which is said to roam the coastline and countryside of East Anglia, one of many such black dogs recorded in folklore across the British Isles.[1][better source needed] Accounts of Black Shuck form part of the folklore of Norfolk, Suffolk, the Cambridgeshire Fens and Essex, and descriptions of the creature's appearance and nature vary considerably; it is sometimes recorded as an omen of death, but, in other instances, is described as companionable.[2][3][4]\n\nAccording to the Oxford English Dictionary, the name Shuck derives from the Old English word scucca 'devil, fiend', perhaps from the root skuh 'to terrify'.[5] The first mention in print of \"Black Shuck\" is by Reverend E. S. Taylor in an 1850 edition of the journal Notes and Queries which describes \"Shuck the Dog-fiend\"; \"This phantom I have heard many persons in East Norfolk, and even Cambridgeshire, describe as having seen as a black shaggy dog, with fiery eyes and of immense size, and who visits churchyards at midnight.\"[6]\n\nAbraham Fleming's account of the appearance of A strange, and terrible wunder in 1577 at Bungay, Suffolk is a famous account of the beast. Images of sinister black dogs have become part of the iconography of the area and have appeared in popular culture.[7] Writing in 1877, Walter Rye stated that Shuck was \"the most curious of our local apparitions, as they are no doubt varieties of the same animal.\"[8]"
-  },
-  {
-    "lat": 487.4619942635426,
-    "lng": 699.8560105069608,
-    "title": "Black Shuck",
-    "category": "Cryptid",
-    "image": "blackshuck.jpg",
-    "desc": "In English folklore, Black Shuck, Old Shuck, Old Shock or simply Shuck is the name given to a ghostly black dog which is said to roam the coastline and countryside of East Anglia, one of many such black dogs recorded in folklore across the British Isles.[1][better source needed] Accounts of Black Shuck form part of the folklore of Norfolk, Suffolk, the Cambridgeshire Fens and Essex, and descriptions of the creature's appearance and nature vary considerably; it is sometimes recorded as an omen of death, but, in other instances, is described as companionable.[2][3][4]\n\nAccording to the Oxford English Dictionary, the name Shuck derives from the Old English word scucca 'devil, fiend', perhaps from the root skuh 'to terrify'.[5] The first mention in print of \"Black Shuck\" is by Reverend E. S. Taylor in an 1850 edition of the journal Notes and Queries which describes \"Shuck the Dog-fiend\"; \"This phantom I have heard many persons in East Norfolk, and even Cambridgeshire, describe as having seen as a black shaggy dog, with fiery eyes and of immense size, and who visits churchyards at midnight.\"[6]\n\nAbraham Fleming's account of the appearance of A strange, and terrible wunder in 1577 at Bungay, Suffolk is a famous account of the beast. Images of sinister black dogs have become part of the iconography of the area and have appeared in popular culture.[7] Writing in 1877, Walter Rye stated that Shuck was \"the most curious of our local apparitions, as they are no doubt varieties of the same animal.\"[8]"
-  },
-  {
-    "lat": 288.37035675245954,
-    "lng": 898.1878500969975,
-    "title": "Brundall ghost",
+    "lat": 581.7074464698767,
+    "lng": 1814.6634523330918,
+    "title": "Brundall Ghost",
     "category": "Haunting",
     "image": "",
     "desc": "The most famous Brundall ghost story is the Bishop’s Barge. According to local folklore, a spectral barge rowed by 28 men appears on the River Yare near the village on the night of June 24 and September 18. Legends claim that this phantom boat brings blessings to any ill people it passes on the water."
+  },
+  {
+    "lat": 335.6545211467859,
+    "lng": 1754.0338021270952,
+    "title": "Black Shuck",
+    "category": "Cryptid",
+    "image": "",
+    "desc": "In English folklore, Black Shuck, Old Shuck, Old Shock or simply Shuck is the name given to a ghostly black dog which is said to roam the coastline and countryside of East Anglia, one of many such black dogs recorded in folklore across the British Isles. \n\nAccounts of Black Shuck form part of the folklore of Norfolk, Suffolk, the Cambridgeshire Fens and Essex, and descriptions of the creature's appearance and nature vary considerably; it is sometimes recorded as an omen of death, but, in other instances, is described as companionable.\n\nAccording to the Oxford English Dictionary, the name Shuck derives from the Old English word scucca 'devil, fiend', perhaps from the root skuh 'to terrify'. The first mention in print of \"Black Shuck\" is by Reverend E. S. Taylor in an 1850 edition of the journal Notes and Queries which describes \"Shuck the Dog-fiend\"; \"This phantom I have heard many persons in East Norfolk, and even Cambridgeshire, describe as having seen as a black shaggy dog, with fiery eyes and of immense size, and who visits churchyards at midnight.\"\n\nAbraham Fleming's account of the appearance of A strange, and terrible wunder in 1577 at Bungay, Suffolk is a famous account of the beast. Images of sinister black dogs have become part of the iconography of the area and have appeared in popular culture.[7] Writing in 1877, Walter Rye stated that Shuck was \"the most curious of our local apparitions, as they are no doubt varieties of the same animal.\""
+  }
+];
+
+const savedTowns = [
+  {
+    "lat": 1183.9862487532628,
+    "lng": 1676.8687927740086,
+    "title": "Cromer",
+    "minZoom": -4
+  },
+  {
+    "lat": 585.3798781911167,
+    "lng": 1660.3334336269186,
+    "title": "Norwich",
+    "minZoom": -4
+  },
+  {
+    "lat": 486.22422171763253,
+    "lng": 2244.5827901574307,
+    "title": "Great Yarmouth",
+    "minZoom": -4
+  },
+  {
+    "lat": 155.70536680601813,
+    "lng": 932.7776311549597,
+    "title": "Thetford",
+    "minZoom": -4
+  },
+  {
+    "lat": 800.2171338836661,
+    "lng": 473.46209929134943,
+    "title": "King's Lynn",
+    "minZoom": -4
+  },
+  {
+    "lat": 794.7084863018058,
+    "lng": 910.7304856255064,
+    "title": "Swaffham",
+    "minZoom": -4
+  },
+  {
+    "lat": 1217.038134244424,
+    "lng": 591.0468754484336,
+    "title": "Hunstanton",
+    "minZoom": -4
+  },
+  {
+    "lat": 1015.0543895762154,
+    "lng": 1043.0133588022263,
+    "title": "Fakenham",
+    "minZoom": -4
+  },
+  {
+    "lat": 1143.5894998196209,
+    "lng": 1381.0695902538437,
+    "title": "Holt",
+    "minZoom": -4
+  },
+  {
+    "lat": 423.79288245654976,
+    "lng": 1428.8384055676588,
+    "title": "Attleborough",
+    "minZoom": -4
+  },
+  {
+    "lat": 480.7155741357723,
+    "lng": 1496.8171042834733,
+    "title": "Wymondham",
+    "minZoom": -4
   }
 ];
